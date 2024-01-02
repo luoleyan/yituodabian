0bc5327bd01f883a2dadf0f098c7b60aad523c0b
EliauK2302 <150649844+Elcited@users.noreply.github.com>
2024-01-02 11:11:08 +0800

    Update AdminDao.java

--- studentperformance/src/main/java/top/fans39/studentperformance/dao/AdminDao.java
+++ studentperformance/src/main/java/top/fans39/studentperformance/dao/AdminDao.java
@@ -3,6 +3,6 @@ package top.fans39.studentperformance.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import top.fans39.studentperformance.entity.Admin;
 
-// 持久层接口，继承Mybatis-Plus
+// 管理员持久层接口，继承Mybatis-Plus
 public interface AdminDao extends BaseMapper<Admin> {
 }
