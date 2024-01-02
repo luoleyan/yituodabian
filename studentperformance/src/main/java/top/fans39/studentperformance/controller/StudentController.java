286732fada95cafa0a0353705718a19d2c860c0e
EliauK2302 <150649844+Elcited@users.noreply.github.com>
2024-01-02 11:03:27 +0800

    Update StudentController.java

--- studentperformance/src/main/java/top/fans39/studentperformance/controller/StudentController.java
+++ studentperformance/src/main/java/top/fans39/studentperformance/controller/StudentController.java
@@ -8,30 +8,37 @@ import top.fans39.studentperformance.common.Result;
 import top.fans39.studentperformance.entity.Student;
 import top.fans39.studentperformance.service.StudentService;
 
+// 管理学生信息的接口
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    // 引入逻辑层
     @Autowired
     private StudentService studentService;
 
+    // 添加学生
     @PostMapping("/add")
     public Result addStudent(@RequestBody Student student) {
         studentService.addStudent(student);
         return Result.success();
     }
 
+    // 根据id删除学生
     @DeleteMapping("/delete/{id}")
     public Result deleteStudent(@PathVariable Integer id) {
         studentService.deleteStudentById(id);
         return Result.success();
     }
 
+    // 更新学生信息
     @PutMapping("/update")
     public Result updateStudent(@RequestBody Student student) {
         studentService.updateStudentById(student);
         return Result.success();
     }
-
+    
+    // 查询学生信息，并将学生信息展示在页面上
     @GetMapping("/selectPage")
     public Result selectStudentPage(@RequestParam(defaultValue ="1" ) Integer pageNum, @RequestParam(defaultValue = "5") Integer pageSize, Student student) {
         // 调用业务层方法，实现分页查询
